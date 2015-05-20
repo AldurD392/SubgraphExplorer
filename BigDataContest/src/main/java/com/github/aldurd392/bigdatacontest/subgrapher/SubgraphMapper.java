3ee12c62ea601893180a205f7d61aa500245ee06
Aldur <adrianodl@hotmail.it>
2015-05-20 19:07:22 +0200

    refactor SubgraphMapper

--- BigDataContest/src/main/java/com/github/aldurd392/bigdatacontest/subgrapher/SubgraphMapper.java
+++ BigDataContest/src/main/java/com/github/aldurd392/bigdatacontest/subgrapher/SubgraphMapper.java
@@ -16,63 +16,83 @@ public class SubgraphMapper extends Mapper<IntArrayWritable, NeighbourhoodMap, I
 
     private final static Random random = new Random();
 
-
     /*
      * Here we return a list containing the most promising neighbours.
      * Those neighbours will be then emitted and on next round will be part
      * of our new subgraph.
      *
      * Note that if probability mode is enabled, then we'll emit those
-     * neighbours only by the given probability.
+     * neighbours only by honouring the given probability.
      */
-    private static List<IntWritable> chooseNodes(NeighbourhoodMap value) {
+    private static Set<IntWritable> chooseNodes(NeighbourhoodMap neighbourhoodMap) {
 
-        HashMap<IntWritable, Integer> counter = new HashMap<>();
-        int length = value.size();
+        /* We'll count here the occurrences of each neighbour in the map */
+        final HashMap<Integer, Integer> counter = new HashMap<>();
 
-        for (Writable writable : value.values()) {
-            IntArrayWritable neighbours = (IntArrayWritable) writable;
+        for (Writable writable_neighbours : neighbourhoodMap.values()) {
+            IntArrayWritable neighbours = (IntArrayWritable) writable_neighbours;
 
-            for (Writable w : neighbours.get()) {
-                IntWritable i = (IntWritable) w;
+            for (Writable writable_neighbour : neighbours.get()) {
+                IntWritable neighbour = (IntWritable) writable_neighbour;
+                Integer neighbour_integer = neighbour.get();
 
-                if (value.containsKey(i)) {
-                    // Avoid counting again nodes already in the subgraph
+                if (neighbourhoodMap.containsKey(neighbour)) {
+                    /*
+                    We avoid counting again nodes already in the subgraph,
+                    aka in the keys of neighbourhoodMap
+                     */
                     continue;
                 }
 
-                Integer c = counter.get(i);
-                if (c == null) {
-                    counter.put(i, 1);
+                Integer count = counter.get(neighbour_integer);
+                if (count == null) {
+                    counter.put(neighbour_integer, 1);
                 } else {
-                    counter.put(i, c + 1);
+                    counter.put(neighbour_integer, count + 1);
                 }
             }
         }
 
+        /*
+        Now we have the count of how many times one of the neighbours is listed in the neighbourhood.
+         */
 
-        TreeMap<Integer, HashSet<IntWritable>> nodes = new TreeMap<>(Collections.reverseOrder());
-        for (Map.Entry<IntWritable, Integer> entry : counter.entrySet()) {
+        /* We'll store here the nodes keyed by their count. */
+        final TreeMap<Integer, HashSet<Integer>> nodes_by_count = new TreeMap<>(Collections.reverseOrder());
+
+        /*
+        Now, we add to the return set only those element whose count is higher than
+        our threshold:
+            euristic factor * length
+         */
+        final int length = neighbourhoodMap.size();
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
 
             if (entry.getValue() >= Main.inputs.getEuristicFactor() * length) {
-                if (nodes.containsKey(entry.getValue())) {
-                    nodes.get(entry.getValue()).add(entry.getKey());
-                } else {
-                    HashSet<IntWritable> relNodes = new HashSet<>();
-                    relNodes.add(entry.getKey());
-                    nodes.put(entry.getValue(), relNodes);
+                HashSet<Integer> countedNodes = nodes_by_count.get(entry.getValue());
+
+                if (countedNodes == null) {
+                    countedNodes = new HashSet<>();
                 }
+
+                countedNodes.add(entry.getKey());
+                nodes_by_count.put(entry.getValue(), countedNodes);
             }
 
         }
 
-        int size = Main.inputs.getOutNodes();
-        ArrayList<IntWritable> finalNodes = new ArrayList<>(size);
+        /*
+        Eventually, we pick the best nodes, in the number we need.
+         */
+        final int outputSize = Main.inputs.getOutNodes();
+        HashSet<IntWritable> finalNodes = new HashSet<>(outputSize);
 
-        for (Map.Entry<Integer, HashSet<IntWritable>> entry : nodes.entrySet()) {
-            for (IntWritable itNode : entry.getValue()) {
-                finalNodes.add(itNode);
-                if (finalNodes.size() == size) {
+        for (Map.Entry<Integer, HashSet<Integer>> entry : nodes_by_count.entrySet()) {
+
+            for (Integer node : entry.getValue()) {
+                finalNodes.add(new IntWritable(node));
+
+                if (finalNodes.size() == outputSize) {
                     return finalNodes;
                 }
             }
@@ -88,23 +108,28 @@ public class SubgraphMapper extends Mapper<IntArrayWritable, NeighbourhoodMap, I
                     Context context)
             throws IOException, InterruptedException {
 
-//    		System.out.println("Mapper input "+ key + " " + value);
         final double p = Main.inputs.probMode();
         final Configuration conf = context.getConfiguration();
         final int round = conf.getInt("round", -1);
 
         if (round == 0) {
+            /* On first round, we emit ourselves too.
+            * We need to do this because Hadoop takes the liberty
+            * to ignore our ReaderMapper on first round. */
             IntWritable node = (IntWritable) key.get()[0];
             context.write(node, value);
         }
 
         for (IntWritable node : chooseNodes(value)) {
             if (p > 0 && random.nextDouble() > p) {
+                /*
+                If probabilistic mode is enabled,
+                we honor the probability and skip emitting the node
+                 */
                 continue;
             }
 
             context.write(node, value);
-//            System.out.println("Mapper output: " + node + " " + value);
         }
     }
 }
